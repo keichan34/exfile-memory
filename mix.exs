543ef484359ac8bc3fcf0ebcf13daf3c39bc3e97
Keitaroh Kobayashi <keita@kbys.me>
2015-12-25 14:58:34 +0900

    Add LICENSE.txt, update mix.exs

--- mix.exs
+++ mix.exs
@@ -2,12 +2,16 @@ defmodule ExfileMemory.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exfile_memory,
-     version: "0.0.1",
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :exfile_memory,
+      version: "0.0.1",
+      elixir: "~> 1.1",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      package: package,
+      description: description
+    ]
   end
 
   # Configuration for the OTP application
@@ -18,7 +22,8 @@ defmodule ExfileMemory.Mixfile do
       mod: {ExfileMemory, []},
       applications: [
         :logger,
-        :crypto
+        :crypto,
+        :exfile
       ]
     ]
   end
@@ -34,7 +39,24 @@ defmodule ExfileMemory.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:exfile, ">= 0.0.0", github: "keichan34/exfile"}
+      {:exfile, ">= 0.0.1"}
     ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Keitaroh Kobayashi"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/keichan34/exfile-memory"
+      }
+    ]
+  end
+
+  defp description do
+    """
+    In-memory (ets) storage backend for Exfile.
+    """
   end
 end
